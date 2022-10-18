a8f2b97b8d5f4b47a1b5682ac3cb22b785dd6d29
matvey733 <morozovmatvej75@gmail.com>
2022-10-18 14:04:23 +0300

    Inputs & Button

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react"
 import Header from "./components/Header"
+import Form from "./components/Form"
 
 export default function App() {
     return (
+        <>
         <Header />
+        <main className="main">
+        <Form />
+        </main>
+        </>
     )
 }
---
+++ src/components/Form.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Form() {
+    return (
+        <div className="form">
+            <div className="inputs">
+                <input />
+                <input />
+            </div>
+            <button className="get-meme-btn">
+                Get a new meme image
+            </button>
+        </div>
+    )
+}
