2f033a49d3df1ab331dcb78fe37f84af646011a1
matvey733 <morozovmatvej75@gmail.com>
2022-10-19 13:42:39 +0300

    Images don't repeat
    Couldn't figure out why recursion didn't work here so I went with 2 ifs

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,31 +5,46 @@ export default function Meme() {
     const memes = data.data.memes;
     function getRandomIndex() { return Math.floor(Math.random() * memes.length ) }
 
+    // "url" is set using getRandomIndex() only initially
     const [url, setUrl] = React.useState(memes[getRandomIndex()].url);
+    const [topText, setTopText] = React.useState();
+    const [bottomText, setBottomText] = React.useState();
 
     function getRandomUrl() {
-        let index = getRandomIndex();
-        let newUrl = memes[index].url;
-        if(newUrl === url) {
-            console.log("URLs are the same. Getting a new one...");
-            getRandomUrl();
-        }; 
+        let newIndex = getRandomIndex();
+        if(memes[newIndex].url === url) {
+            newIndex = getRandomIndex(); 
+            console.log("hi");  
+            if(memes[newIndex].url === url) {
+                newIndex = getRandomIndex();
+            }
+        } 
+        let newUrl = memes[newIndex].url;
+        
+        console.log(document.querySelector(".top-text-input").value);
+        console.log(document.querySelector(".bottom-text-input").value);
+
         setUrl(newUrl);
+        setTopText(document.querySelector(".top-text-input").value)
+        setBottomText(document.querySelector(".bottom-text-input").value)
     }
 
     return (
         <main className="meme">
             <form className="form">
-                <input placeholder="Top text"/>
-                <input placeholder="Bottom text"/>
+                <input className="top-text-input" placeholder="Top text"/>
+                <input className="bottom-text-input" placeholder="Bottom text"/>
             </form>
             <button 
             className="get-meme-btn"
             onClick={getRandomUrl}>
                 Get a new image
             </button>
-
-            <img src={url} className="meme-img" alt="" />
+            <div className="image-text-wrapper">
+                <div className="top-text">{topText}</div>
+                <div className="bottom-text">{bottomText}</div>
+                <img src={url} className="meme-img" alt="" />
+            </div>
         </main> 
     )
 }
