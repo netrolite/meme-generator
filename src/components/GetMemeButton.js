bb52778029b47108e4b044726e3e2aa2fcaacf5e
matvey733 <morozovmatvej75@gmail.com>
2022-10-26 18:33:44 +0300

    Update GetMemeButton.js

--- src/components/GetMemeButton.js
+++ src/components/GetMemeButton.js
@@ -2,11 +2,15 @@ import React from "react";
 
 export default function GetMemeButton(props) {
     return (
+        <>
         <button 
             className="get-meme-btn"
             onClick={props.getRandomUrl}
         >
             Get a new image
         </button>
+        {/* fNi1JyfM3NH7nm--hfEezvqltJN8ULsO_gKrV4wvjZA
+        NLd8dBT3ICqziJ8TcuKl40Gg_rFcFjTPAM-K4fSsA5c */}
+        </>
     )
 }
